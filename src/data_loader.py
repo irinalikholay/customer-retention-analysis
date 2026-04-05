74ef07c82b49e105df6875c2b72f73f1f42596d4
Iryna Lykholai <lihollaykaa@gmail.com>
2026-04-05 22:34:57 +0200

    Data cleaning and preprocessing

--- src/data_loader.py
+++ src/data_loader.py
@@ -38,5 +38,44 @@ def explore_data():
     return df
 
 
+### CLEAN DATA ###
+
+def clean_data(df):
+    print("\nStarting data cleaning...\n")
+
+    print(f"initian shape: {df.shape}")
+
+    #Remove missing customer id
+    df = df.dropna(subset=["Customer ID"])
+    print(f"After removing missing Customer ID: {df.shape}")
+
+    #Remove cancelled invoices
+    df = df[~df["Invoice"].astype(str).str.startswith("C")]
+    print(f"After removing cancelled invoices: {df.shape}")
+
+    #Remove negative ar zero quantity
+    df = df[df["Quantity"] > 0]
+    print(f"After removing negative quantity: {df.shape}")
+
+    #Remove zero or negative price
+    df = df[df["Price"] > 0]
+    print(f"After removing zero/negative price: {df.shape}")
+
+    #Convert InvoiceDate to datetime
+    df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"])
+    
+    #convert Customer Id to int
+    df["Customer ID"] = df["Customer ID"].astype(int)
+
+    #Create Revenue column
+    df["Revenue"] = df["Quantity"] * df["Price"]
+
+    print(f"\nFinal cleaned shape: {df.shape}")
+
+    return df
+
+
 if __name__ == "__main__":
-    explore_data()
+    df = explore_data()
+    df_clean = clean_data(df)
+
